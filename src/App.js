366180b26130c690f05d31c158efa7082026f9b9
Nick Stradford <triplednick@gmail.com>
2017-03-01 07:27:26 -0600

    Setting up text area input functionality

--- src/App.js
+++ src/App.js
@@ -55,51 +55,111 @@ const showState = () => console.log(store.getState());
  * @return { Promise } - A promise object that will eventually contain our response message.
  */
 const callServer = message => {
-  return axios.post('/message', {
-    message: message
-  });
+  return axios
+    .post('/message', {
+      message: message
+    })
+    .then(function(response) {
+      const data = response.data;
+      const cleverOutput = data.clever_output;
+
+      sendResposeDispatch(cleverOutput);
+      removeFromMessageQueueDispatch(message);
+      sendMessageDispatch(message);
+      showState();
+    })
+    .catch(function(error) {
+      console.log('in util class with error');
+      console.log(error);
+    });
 };
 
 class App extends Component {
   constructor(props) {
     super(props);
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleKeyUp = this.handleKeyUp.bind(this);
+
     this.state = {
       responses: [],
       messageQueue: [],
-      messagesHandled: []
+      messagesHandled: [],
+      inputMessage: ''
     };
   }
   componentDidMount() {
     console.log('componentDidMount()');
+  }
+  handleSubmit(e) {
+    e.preventDefault();
 
-    const message = 'Be quiet!!!';
-    showState();
-    addToMessageQueueDispatch(message);
-    showState();
+    const inputMessage = this.state.inputMessage;
 
-    callServer(message)
-      .then(function(response) {
-        const data = response.data;
-        const cleverOutput = data.clever_output;
+    if (e.keyCode === 13 && !e.shiftKey) {
+      console.log(`Input message: ${this.state.inputMessage}`);
+      if (inputMessage.length > 0) {
+        //callServer(inputMessage);
+      }
+    }
+  }
+  handleChange(e) {
+    this.setState({ inputMessage: e.target.value });
+  }
+  handleKeyUp(e) {
+    const getCaret = el => {
+      if (el.selectionStart) {
+        return el.selectionStart;
+      } else if (document.selection) {
+        el.focus();
+        var r = document.selection.createRange();
+        if (r == null) {
+          return 0;
+        }
+        var re = el.createTextRange(), rc = re.duplicate();
+        re.moveToBookmark(r.getBookmark());
+        rc.setEndPoint('EndToStart', re);
+        return rc.text.length;
+      }
+      return 0;
+    };
 
-        sendResposeDispatch(cleverOutput);
-        removeFromMessageQueueDispatch(message);
-        sendMessageDispatch(message);
-        showState();
-      })
-      .catch(function(error) {
-        console.log('in util class with error');
-        console.log(error);
-      });
+    if (e.keyCode === 13) {
+      var content = this.state.inputMessage;
+      var caret = getCaret(this.getDOMNode());
+      if (e.shiftKey) {
+        this.value = content.substring(0, caret - 1) +
+          '\n' +
+          content.substring(caret, content.length);
+        e.stopPropagation();
+      } else {
+        this.value = content.substring(0, caret - 1) +
+          content.substring(caret, content.length);
+      }
+    }
   }
   render() {
     return (
-      <div className="App">
-        <div className="chat-box">
-          chat box
-        </div>
-        <div className="input-field">
-          input field
+      <div className="app-box">
+        <div className="chat-box-container">
+          <div className="chat-box">
+            chat box
+          </div>
+          <div className="input-area">
+            <form onSubmit={this.handleSubmit}>
+              <div className="text-area">
+                <label>
+                  <textarea
+                    value={this.state.inputMessage}
+                    onChange={this.handleChange}
+                    onKeyUp={this.handleKeyUp}
+                    className="message-area"
+                  />
+                </label>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     );
